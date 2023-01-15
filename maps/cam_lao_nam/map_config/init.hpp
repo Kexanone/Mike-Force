260eda17482b9037714cd8b45261b1374feebee5
Kex <kexanone@gmail.com>
2023-01-15 03:40:05 +0100

    Update cam_lao_nam map

--- maps/cam_lao_nam/map_config/init.hpp
+++ maps/cam_lao_nam/map_config/init.hpp
@@ -1,5 +1,5 @@
 class map_config {
-	starting_zones[] = {"zone_ba_ria", "zone_ban_hoang"};
+	starting_zones[] = {"zone_ban_hoang", "zone_cu_chi", "zone_hue", "zone_fob_mai_loc"};
 	max_camps_per_zone = 5;
 	max_aa_per_zone = 10;
 	max_artillery_per_zone = 3;
